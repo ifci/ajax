5786b7ac5589ee645b72aafc335347a7b09128dd
ifci <1020358873@qq.com>
2015-05-18 23:10:54 +0800

    2015/5/18

---
+++ demo/check.php
@@ -0,0 +1,6 @@
+<?php
+$num = $_POST['num'];
+if($num%2 == 0)
+    echo "偶数";
+else
+    echo "奇数";
---
+++ demo/check_f.php
@@ -0,0 +1,6 @@
+<?php
+$num = $_POST['num'];
+if($num > 0)
+    echo "大于0";
+else
+    echo "小于0";
